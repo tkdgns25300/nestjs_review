48db57273b1b2c233da0b1abbb66b4e59c36055e
이상훈 <leesanghun@isanghun-ui-MacBookAir.local>
2024-01-26 02:42:04 +0900

    feat: get/create board

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from "@nestjs/common";
+import { BoardsService } from "./boards.service";
+import { Board } from "./board.model";
 
-@Controller('boards')
-export class BoardsController {}
+@Controller("boards")
+export class BoardsController {
+	constructor(private boardsService: BoardsService) {}
+
+	@Get("/")
+	getAllBoard(): Board[] {
+		return this.boardsService.getAllBoards();
+	}
+
+	@Post("/")
+	createBoard(@Body("title") title: string, @Body("description") description: string): Board {
+		return this.boardsService.createBoard(title, description);
+	}
+}

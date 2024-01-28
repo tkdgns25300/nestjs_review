98c15655a73f64f7b8578e25e4af86443c5dcac6
이상훈 <leesanghun@isanghun-ui-MacBookAir.local>
2024-01-28 16:51:14 +0900

    feat: log

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -3,6 +3,7 @@ import {
 	Controller,
 	Delete,
 	Get,
+	Logger,
 	Param,
 	ParseIntPipe,
 	Patch,
@@ -23,6 +24,7 @@ import { User } from "src/auth/user.entity";
 @Controller("boards")
 @UseGuards(AuthGuard())
 export class BoardsController {
+	private logger = new Logger("BoardsController");
 	constructor(private boardsService: BoardsService) {}
 
 	@Get("/:id")
@@ -33,6 +35,9 @@ export class BoardsController {
 	@Post("/")
 	@UsePipes(ValidationPipe)
 	createboard(@Body() createBoardDto: CreateBoardDto, @GetUser() user: User): Promise<Board> {
+		this.logger.verbose(
+			`User "${user.username}" creating a new board. Payload: ${JSON.stringify(createBoardDto)}`,
+		);
 		return this.boardsService.createBoard(createBoardDto, user);
 	}
 
@@ -51,6 +56,7 @@ export class BoardsController {
 
 	@Get("/")
 	getAllBoard(@GetUser() user: User): Promise<Board[]> {
+		this.logger.verbose(`User "${user.username}" trying to get all boards`);
 		return this.boardsService.getAllBoards(user);
 	}
 
